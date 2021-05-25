06939951b6b0c81b07890e729956074c91d276e7
codeonium <codeon2020@gmail.com>
2021-05-25 21:41:36 +0100

    initial commit

---
+++ src/test/java/com/codeclan/example/guitarShop/GuitarShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.guitarShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuitarShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
